6ab371bc1b5f38cc0534e79ee3a0f4a21381cd65
madhu5403 <54945549+madhu5403@users.noreply.github.com>
2023-01-28 23:47:10 +0530

    Add files via upload

---
+++ Scartch/App.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import CardList from "./src/components/CardList"
+
+export default function App() {
+  return (
+    <>
+      <CardList />
+    </>
+  )
+}
---
+++ Scartch/src/components/CardDetails.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+import { useParams } from "react-router-dom"
+
+export default function CardDetails() {
+  const id = useParams()
+  return <div>CardDetails</div>
+}
